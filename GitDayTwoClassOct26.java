78051b34b24e076801a83ad3f8b01362c5b7fdee
AnandhaGanapathyS <149878246+AnandhaGanapathyS@users.noreply.github.com>
2023-12-29 23:08:34 +0530

    Add files via upload

---
+++ GitDayTwoClassOct26.java
@@ -0,0 +1,40 @@
+package org.git;
+
+public class GitDayTwoClassOct26 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//DataType varName = Value;
+		
+				byte age = 28; //2 digits
+				System.out.println("Employee age:"+age);
+				
+				short atmPin = 2345; //4 digit
+				System.out.println("Employee atmPin:"+atmPin);
+				
+				int landLineNo = 987654321; //9 digits
+				System.out.println("Employee LandLine No:"+landLineNo);
+				
+				long accNo = 7799678809877l; //19 digit
+				System.out.println("Employee accNo:"+accNo);
+				//after point no value is there means it default take as .00 and after point more than  two digit must use f
+				float salary = 47289.4428997676f;
+				System.out.println("Employee salary:"+ salary);
+				//double values returns same values after float point
+				double weight = 283229489.4939580;
+				System.out.println("Employee weight:"+weight);
+				
+				char gender = 'M';
+				System.out.println("Employee gender: "+gender);
+				
+				String email = "ganapathyanandha4@gmail.com";
+				System.out.println("Employee email id:"+email);
+				
+				boolean info = true;
+				System.out.println("All the above information are " + info + " to the best of my knowledge");
+				
+				
+
+	}
+
+}
